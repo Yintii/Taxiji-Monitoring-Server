555b827adb5e9a4f6315b5b1e58257955cbfe46a
yintii <keleheart1@gmail.com>
2024-02-01 02:41:40 +0000

    working to make the server and the extension talk to each other but I am not 100% sure on how to do it yet

--- log_wallet.js
+++ log_wallet.js
@@ -1,25 +1,76 @@
 import Web3 from 'web3';
+import WebSocket from 'ws'
+import { ethers } from 'ethers';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import express from 'express'
+import http from 'http'
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const liveApiUrl = `wss://eth-mainnet.g.alchemy.com/v2/${process.env.LIVE_API_KEY}`;
 const sepoliaApiUrl = `wss://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_API_KEY}`;
 const web3 = new Web3(sepoliaApiUrl);
 
 const targetWalletAddress = process.argv[2].toLowerCase();
 
-let subscription = (await web3.eth.subscribe('pendingTransactions'));
+
+const subscription = (await web3.eth.subscribe('pendingTransactions'));
+
+
 
 console.log('Listening for transactions on wallet: ', targetWalletAddress);
 
 subscription.on('data', async (txHash) => {
     try {
         const tx = await web3.eth.getTransaction(txHash);
-        if(tx.to === targetWalletAddress || tx.from === targetWalletAddress){
-            console.log('Transaction detected: ', txHash);
+        
+        // Check if tx is not undefined and has the value field
+        if (tx && tx.value) {
+            if (tx.to === targetWalletAddress || tx.from === targetWalletAddress) {
+                console.log('Transaction detected: ', txHash);
+                console.log('Transaction data: ', tx);
+                console.log('Transaction amount: ', ethers.formatEther(tx.value));
+                console.log('Transaction withholding amount: ', ethers.formatEther(BigInt(tx.value) * BigInt(2) / BigInt(10)));
+
+		const withholdingAmt = ethers.formatEther(BigInt(tx.value) * BigInt(2) / BigInt(10));
+		const provider = ethers.getDefaultProvider(sepoliaApiUrl);
+		const signer = new ethers.Wallet(process.env.SERVER_PRIVATE_KEY, provider);
+
+		const withholdingTransaction = {
+			to: '0x66D96228559500a475Fd54bB673C00f35ca91a59',
+			value: ethers.parseEther(withholdingAmt)
+		}
+
+		console.log(withholdingTransaction);
+
+		try {
+		  console.log(wss.clients.size);
+		  wss.clients.forEach(client => {
+			console.log(client);
+			if (client.readyState === WebSocket.OPEN){
+				const msg = JSON.stringify({
+					signal: 'show_popup',
+					withholdingTransaction
+				})
+				client.send(msg);
+			}
+		  });
+		}catch (error){
+			console.error('Error showing popup');
+		}
+		     
+            }
         }
-    }
-    catch(error){
-        if (error.code === 430) return;
+    } catch (error) {
+        if (error.code === 430 || error.code === 101) return;
         console.error('Error on transaction detection: ', error);
     }
 });
+
+server.listen(3000, ()=>{
+	console.log('Listening on 3000');
+})
---
+++ test.js
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+console.log(ethers.parseEther('1.5'))
+
