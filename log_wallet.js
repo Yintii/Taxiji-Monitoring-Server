99370e3d882cc686b388bb108b1dbc4be577ae6f
yintii <keleheart1@gmail.com>
2024-02-09 13:06:34 -0800

    messed up the logic here because we're calling the contract now, not sending directly to withholding wallets

--- log_wallet.js
+++ log_wallet.js
@@ -6,6 +6,7 @@ dotenv.config();
 const sepoliaApiUrl = `wss://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_API_KEY}`;
 const web3 = new Web3(sepoliaApiUrl);
 
+const WITHHOLDING_WALLET = '0xA0AD8Cda8cA5c43403B03A4C21181b0f6768A580'
 
 const targetWalletAddress = process.argv[2];
 
@@ -18,27 +19,18 @@ console.log('Listening for transactions on wallet: ', targetWalletAddress);
 subscription.on('data', async (txHash) => {
     try {
         const tx = await web3.eth.getTransaction(txHash);
-
-        
         // Check if tx is not undefined and has the value field
         if (tx && tx.value) {
+			if(tx.to === WITHHOLDING_WALLET) return;
 			if (tx.to === targetWalletAddress || tx.from === targetWalletAddress) {
-				console.log('Transaction to: ', tx.to);
-				console.log('Sepolia withholding wallet: ', process.env.SEPOLIA_WITHHOLDING_WALLET);
-
-				console.log('Transaction is going to sepolia withholding wallet: ', tx.to === process.env.SEPOLIA_CONTRACT_ADDRESS);
-				if (tx.to === process.env.SEPOLIA_CONTRACT_ADDRESS || tx.to === process.env.SEPOLIA_WITHHOLDING_WALLET) return
-
 				const withholdingAmt = ethers.formatEther(BigInt(tx.value) * BigInt(2) / BigInt(10));
 				const withholdingTransaction = {
 					from: targetWalletAddress,
 					to: process.env.SEPOLIA_WITHHOLDING_WALLET,
 					value: ethers.parseEther(withholdingAmt).toString(),
 				};
-				console.log('Attempting to log withholding transaction: ', withholdingTransaction);
 				try {
 					pendingTransactions.push(withholdingTransaction);
-					console.log("Pending transactions: ", pendingTransactions);
 					process.send(pendingTransactions);
 				}catch (error){
 					console.error('Error sending transaction data: ', error);
