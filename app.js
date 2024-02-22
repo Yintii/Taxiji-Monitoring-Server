f5f460a6481c5670df32d9faca976b9ecdfb3cdb
yintii <keleheart1@gmail.com>
2024-02-22 23:26:51 +0000

    it does not seem to know the wallet address is part of the walletProcesses mapping

--- app.js
+++ app.js
@@ -132,23 +132,30 @@ app.post('/api/wallet_submit/', (req, res) => {
 });
 
 //destroy wallet
-app.post('/api/wallet_stop/', (req, res) => {
-  const { wallet_to_monitor} = req.body;
-  
-  if(!walletProcesses.has(wallet_to_monitor)){
-    return res.status(400).send('No running process found for this wallet');
-  }
+app.post('/api/wallet_stop/', async (req, res) => {
+  const { wallet_address } = req.body;
+  console.log('We are attempting to stop wallet: ', wallet_address); 
+  console.log('Wallet Processes: ', walletProcesses);
+  console.log('The wallet address is in the wallet processes: ', !walletProcesses.has(wallet_address));
+  //if(!walletProcesses.has(wallet_address)){
+  //  return res.status(400).send('No running process found for this wallet');
+  //}
 
-  const process = walletProcesses.get(wallet_to_monitor);
+  //try{
+  //console.log('Begining of try block');
+  //const process = walletProcesses.get(wallet_address);
  
-  process.kill()
+  //process.kill()
 
-  walletProcesses.delete(wallet_to_monitor);
-  console.log('Wallet processes after deleting the process in question: ', walletProcesses);
-  //fs.writeFileSync('./processes/wallets_to_monitor.json', JSON.stringify([...walletProcesses]));
+  //walletProcesses.delete(wallet_address);
+  //console.log('Wallet processes after deleting the process in question: ', walletProcesses);
+  ////fs.writeFileSync('./processes/wallets_to_monitor.json', JSON.stringify([...walletProcesses]));
 
-  console.log(`Process for ${wallet_to_monitor} stopped successfully`);
-  res.status(200).send(`Process for ${wallet_to_monitor} stopped successfully.`);
+  //console.log(`Process for ${wallet_to_monitor} stopped successfully`);
+  //res.status(200).send(`Process for ${wallet_to_monitor} stopped successfully.`);
+  //}catch (error){
+  //  console.error("There was an error getting rid of the wallet: ", error);	
+  //}
 });
 
 //a simple route that will show what the pending transactions are
